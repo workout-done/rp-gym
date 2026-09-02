ecad1abc5008bbd2225c37cf371aa4e8b84c5d3f
skdysla <skdysla@gmail.com>
2026-09-02 14:54:32 +0900

    feat: Health Service 기본 모듈 및 실행 환경 구성

---
+++ health-service/src/main/java/com/workoutdone/rpgym/health/HealthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.workoutdone.rpgym.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthServiceApplication.class, args);
+    }
+}
