d4a74bb224a1f49cf0d63ad85e4e5a819fd4178a
jiho0107 <ghkdwlgh123@naver.com>
2026-09-03 19:24:48 +0900

    comment(user): SignUpService에 주석 추가

--- user-service/src/main/java/com/workoutdone/rpgym/user/application/service/SignUpService.java
+++ user-service/src/main/java/com/workoutdone/rpgym/user/application/service/SignUpService.java
@@ -19,10 +19,13 @@ public class SignUpService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    //회원가입
     public SignUpResult signUp(SignUpCommand command) {
+        // 이메일 및 닉네임 중복 여부 확인
         validateDuplicateEmail(command.getEmail());
         validateDuplicateNickname(command.getNickname());
 
+        // 입력받은 평문 비밀번호를 BCrypt로 해싱하여 저장
         String encodedPassword = passwordEncoder.encode(command.getRawPassword());
 
         User user = User.create(
@@ -37,12 +40,14 @@ public class SignUpService {
         return SignUpResult.from(savedUser);
     }
 
+    //이메일 중복 확인 메서드
     private void validateDuplicateEmail(String email) {
         if (userRepository.existsByEmailAndDeletedAtIsNull(email)) {
             throw new BaseException(UserErrorCode.EMAIL_DUPLICATED);
         }
     }
 
+    //닉네임 중복 확인 메서드
     private void validateDuplicateNickname(String nickname) {
         if (userRepository.existsByNicknameAndDeletedAtIsNull(nickname)) {
             throw new BaseException(UserErrorCode.NICKNAME_DUPLICATED);
