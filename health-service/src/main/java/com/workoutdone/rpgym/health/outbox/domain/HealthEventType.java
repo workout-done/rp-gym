6ead8d40e4f19019fbddd3926757544859629791
Jinhyelim <254581953+Jinhyelim@users.noreply.github.com>
2026-09-04 15:17:57 +0900

    HealthEventType 불필요한 @RequiredArgsConstructor삭제

--- health-service/src/main/java/com/workoutdone/rpgym/health/outbox/domain/HealthEventType.java
+++ health-service/src/main/java/com/workoutdone/rpgym/health/outbox/domain/HealthEventType.java
@@ -1,8 +1,5 @@
 package com.workoutdone.rpgym.health.outbox.domain;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public enum HealthEventType {
     HEALTH_ACTIVITY_SYNCED, QUEST_SUGGESTED, DAILY_GOAL_COMPLETED
 }
