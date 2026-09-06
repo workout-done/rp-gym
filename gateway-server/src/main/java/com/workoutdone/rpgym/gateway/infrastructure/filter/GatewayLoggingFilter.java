64fa0d21d310b40feed74f8456b858ea5d973431
skdysla <skdysla@gmail.com>
2026-09-06 15:28:41 +0900

    refactor(gateway): 요청 추적을 위한 traceId 로깅 추가

--- gateway-server/src/main/java/com/workoutdone/rpgym/gateway/infrastructure/filter/GatewayLoggingFilter.java
+++ gateway-server/src/main/java/com/workoutdone/rpgym/gateway/infrastructure/filter/GatewayLoggingFilter.java
@@ -1,12 +1,15 @@
 package com.workoutdone.rpgym.gateway.infrastructure.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -16,21 +19,32 @@ public class GatewayLoggingFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String method = exchange.getRequest().getMethod().name();
         String path = exchange.getRequest().getPath().value();
+        String traceId = UUID.randomUUID().toString();
         long startTime = System.nanoTime();
 
-        log.info("Gateway request: {} {}", method, path);
+        MDC.put("traceId", traceId);
+
+        log.info(
+                "[{}] Gateway request: {} {}",
+                traceId,
+                method,
+                path
+        );
 
         return chain.filter(exchange)
                 .doFinally(signal -> {
                     long latency = (System.nanoTime() - startTime) / 1_000_000;
 
                     log.info(
-                            "Gateway response: {} {} {}ms {}",
+                            "[{}] Gateway response: {} {} {}ms {}",
+                            traceId,
                             method,
                             path,
                             latency,
                             exchange.getResponse().getStatusCode()
                     );
+
+                    MDC.remove("traceId");
                 });
     }
 
